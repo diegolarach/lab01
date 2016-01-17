274d3757319cefa384c5478cf66e6a7efbf6b3c0
diegolarach <diego.larach5@gmail.com>
2016-01-17 14:44:58 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -7,5 +7,24 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello world!\n");
-	return 0;
+	printf("Written by Diego Larach\n");
+
+
+     /* This section includes a sum */
+         int x,y,z;
+ 	     x=10;
+         y=20;
+         z= x + y;
+         printf("x is %d,y is %d,z is %d\n",x,y,z);
+     
+
+         /* This expression will help us compute the volume of a box and will print out the height, width, depth, and volume. */
+           int h,d,w,v;
+           h=11;
+           d=14;
+           w=7;
+           v= h*d*w;
+           printf("h is %d,d is %d,w is %d, v is %d\n",h,d,w,v);
+              return 0;
 }
+           
